4e8cdd7b49c5eeab5bd75aa4dfb23cabbbffc209
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-07 06:44:24 +0530

    html set in response

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("", (req, res) => {
-  res.send("hello express");
+  res.send("<h1>Weather</h1>");
 });
 app.get("/help", (req, res) => {
   res.send("Help page");
