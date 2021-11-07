4a20a504d0ed211ff8c0790aa62bff85ee657c08
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-07 16:50:37 +0530

    dynamic page setup

--- src/app.js
+++ src/app.js
@@ -9,7 +9,22 @@ app.set("view engine", "hbs");
 
 app.use(express.static(publicDir));
 app.get("", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Weather App",
+    name: "chinmay",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Weather forecasting",
+    country: "India",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    mobile: 894834939,
+    time: "9am to 9pm",
+  });
 });
 app.get("/weather", (req, res) => {
   res.send({ forecast: "sunny" });
