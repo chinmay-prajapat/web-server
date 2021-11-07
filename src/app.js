7fde67949377fc8f69a86245f6a651498709c84a
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-07 06:39:17 +0530

    Routes created

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const app = express();
 app.get("", (req, res) => {
   res.send("hello express");
 });
+app.get("/help", (req, res) => {
+  res.send("Help page");
+});
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+app.get("/weather", (req, res) => {
+  res.send("Weather");
+});
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
