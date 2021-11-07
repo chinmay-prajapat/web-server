251e06fe2f87ca3a5b01eaa74c378150c97d0bd4
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-07 16:25:23 +0530

    dynamic template library setup

--- src/app.js
+++ src/app.js
@@ -5,8 +5,12 @@ console.log();
 const app = express();
 
 const publicDir = path.join(__dirname, "../public");
-app.use(express.static(publicDir));
+app.set("view engine", "hbs");
 
+app.use(express.static(publicDir));
+app.get("", (req, res) => {
+  res.render("index");
+});
 app.get("/weather", (req, res) => {
   res.send({ forecast: "sunny" });
 });
