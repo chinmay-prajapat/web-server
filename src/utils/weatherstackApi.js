444a48f0e59152a85de8ca3cd081df071982862d
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-10 23:18:29 +0530

    index file edited

--- src/utils/weatherstackApi.js
+++ src/utils/weatherstackApi.js
@@ -1,7 +1,7 @@
 const request = require("postman-request");
 
 const weatherForecast = (long, lat, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK}&query=${long},${lat}`; //units=f(f=fahernheit)
+  const url = `http://api.weatherstack.com/current?access_key=0098daca9d36923459c45f3b800f2619&query=${long},${lat}`; //units=f(f=fahernheit)
   request({ url, json: true }, (err, res) => {
     if (err) {
       callback("unable to connect", undefined);
